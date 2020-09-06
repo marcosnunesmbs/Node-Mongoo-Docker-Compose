465316eef8d07708ba66795745da5627267752bc
Marcos Nunes <marcosnunesmbs@gmail.com>
2020-09-06 16:31:17 -0300

    last changes

--- back/app.js
+++ back/app.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const restful   = require('node-restful')
 const mongoose  = restful.mongoose
 const app = express()
+
+app.use(bodyParser.json())
+const port = 3000
 
 // Database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
 //teste
-app.get('/', (req, res, next) => res.send('Backendo done...'))
+app.get('/', (req, res, next) => res.send('Backend done...'))
 
 //start
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`Server is running at localhost:${port}`)
+  })
